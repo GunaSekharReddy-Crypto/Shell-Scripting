da5752b657883c2357f7a43a88aa1f4da50804df
Maramreddy Tittigandla Gunasekhar Reddy (Nokia) <maramreddy.tittigandla_gunasekhar_reddy@nokia.com>
2026-02-02 10:10:46 +0530

    First Commit - Variables

---
+++ Shell-Scripting/variable-1.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# A simple script to demonstrate basic shell commands
+# and variable usage in bash.
+# Define a variable
+GREETING="Hello, World!"
+# Print the variable to the console
+echo $GREETING
+# Define another variable
+NAME="Alice"
+# Print a personalized greeting
+echo "Hello, $NAME! Welcome to the bash scripting tutorial."
+# Perform a simple arithmetic operation
+NUM1=5
+NUM2=10
+SUM=$((NUM1 + NUM2))
+# Print the result of the arithmetic operation
+echo "The sum of $NUM1 and $NUM2 is: $SUM"
+# End of script
